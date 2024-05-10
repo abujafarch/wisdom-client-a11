7126829142c056e73b597fa996b0f6ec5cb601d0
AbujafarCh <357jafar@gmail.com>
2024-05-10 16:51:47 +0600

    First setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKK4z8cUQzLcdd_KX_IQzu8_rgimmM8gE",
+  authDomain: "wisdom-cca7e.firebaseapp.com",
+  projectId: "wisdom-cca7e",
+  storageBucket: "wisdom-cca7e.appspot.com",
+  messagingSenderId: "1045904570139",
+  appId: "1:1045904570139:web:2a35171639916b6096c5be"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
